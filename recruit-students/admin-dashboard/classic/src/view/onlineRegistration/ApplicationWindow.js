cecc96121a386ca8d49cdb53fa2fb8261753483a
lixia233 <klx_233@163.com>
2018-12-23 23:18:15 +0800

    学生申请信息

--- recruit-students/admin-dashboard/classic/src/view/onlineRegistration/ApplicationWindow.js
+++ recruit-students/admin-dashboard/classic/src/view/onlineRegistration/ApplicationWindow.js
@@ -16,8 +16,8 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
         'Ext.form.Panel'
     ],
 
-    height:655,
-    width:800,
+    height:540,
+    width:860,
     scrollable:true,
     bodyPadding:10,
     closable: true,//如果为True，则显示“关闭”工具按钮并允许用户关闭面板
@@ -32,7 +32,7 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
         title:'请填写申请信息',
         titleAlign:'center',
         layout:'card',
-        height:550,
+        height:400,
         fieldDefaults:{
             border:false,
             labelAlign:'right',
@@ -40,6 +40,8 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
             defaultType:'textfield'
         },
         defaultListenerScope:true, //如果true，此组件将是使用字符串名称指定的事件的默认范围（此指针），以便可以动态解析范围。如果指定了控制器，该组件将自动成为defaultListenerScope 。
+        defaultFocus: 'textfield',
+        defaultButton: 'submitButton',  //用作此Panel的默认按钮的组件的引用名称。
         items:[{
             id:'application-0',
             xtype:'fieldset',
@@ -48,57 +50,111 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
                 anchor:'100%'
             },
             items:[{
-                xtype: 'textfield',
-                fieldLabel:'学生姓名',
-                name:'student_name',
-                allowBlank:false
+                xtype:'fieldcontainer',
+                layout:'hbox',
+                defaultType:'textfield',
+                items:[{
+                    xtype: 'textfield',
+                    fieldLabel:'学生姓名',
+                    labelWidth: 110,
+                    name:'student_name',
+                    allowBlank:false,
+                    msgTarget:'side',
+                    flex:1
+                },{
+                    xtype:'radiogroup',
+                    fieldLabel:'学生性别',
+                    labelWidth: 110,
+                    // name:'student_sex',
+                    allowBlank:false,
+                    msgTarget:'side',
+                    items:[{
+                        xtype:'radiofield',
+                        name:'student_sex',
+                        boxLabel:'男',
+                        inputValue:'男'
+                    },{
+                        xtype:'radiofield',
+                        name:'student_sex',
+                        boxLabel:'女',
+                        inputValue:'女'
+                    }],
+                    flex:1
+                }]
             },{
                 xtype:'textfield',
                 fieldLabel:'学生身份证号码',
+                labelWidth: 110,
                 name:'student_identificationNumber',
                 allowBlank:false,
+                msgTarget:'side',
                 regex:/^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/,
                 regexText:'请输入学生的18位有效身份证号码'
             },{
-                xtype:'radiogroup',
-                fieldLabel:'学生性别',
-                name:'student_sex',
-                allowBlank:false,
+                xtype:'fieldcontainer',
+                layout:'hbox',
+                defaultType:'textfield',
                 items:[{
-                    boxLabel:'男',
-                    inputValue:'男'
+                    xtype:'datefield',
+                    fieldLabel:'学生出生日期',
+                    labelWidth: 110,
+                    name:'student_birthday',
+                    allowBlank:false,
+                    msgTarget:'side',
+                    format:'Y/m/d',
+                    value:new Date(),
+                    maxValue:new Date(),
+                    flex:1
                 },{
-                    boxLabel:'女',
-                    inputValue:'女'
+                    xtype:'combobox',
+                    fieldLabel:'学生民族',
+                    labelWidth: 110,
+                    name:'student_nationality',
+                    allowBlank:false,
+                    msgTarget:'side',
+                    emptyText: '---请选择---',
+                    displayField:'num', //要绑定到此ComboBox 的基础数据字段名称。
+                    valueField:'nationalityType',
+                    store:{
+                        type:'nationalityStore'
+                    },
+                    queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
+                    minChars: 0, //用户在自动完成和typeAhead激活之前必须输入的最小字符数。
+                    typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
+                    forceSelection:true,  //防止自由格式值并将其限制为列表中的项目
+                    flex:1
+                },{
+                    xtype:'combobox',
+                    fieldLabel:'父母婚姻状况',
+                    labelWidth: 110,
+                    name:'student_parentalMarriage',
+                    allowBlank:false,
+                    msgTarget:'side',
+                    emptyText: '---请选择---',
+                    displayField:'name', //要绑定到此ComboBox 的基础数据字段名称。
+                    valueField:'value',
+                    store:Ext.create('Ext.data.Store',{
+                        fields:["name","value"],
+                        data:[
+                            {name:'未婚',value:'未婚'},
+                            {name:'已婚(有配偶)',value:'已婚'},
+                            {name:'丧偶',value:'丧偶'},
+                            {name:'离婚',value:'离婚'}
+                        ]
+                    }),
+                    queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
+                    minChars: 0, //用户在自动完成和typeAhead激活之前必须输入的最小字符数。
+                    typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
+                    forceSelection:true,  //防止自由格式值并将其限制为列表中的项目
+                    flex:1
                 }]
-            },{
-                xtype:'datefield',
-                fieldLabel:'学生出生日期',
-                name:'student_birthday',
-                allowBlank:false,
-                format:'Y/m/d',
-                value:new Date(),
-                maxValue:new Date()
-            },{
-                xtype:'combobox',
-                fieldLabel:'学生民族',
-                name:'student_nationality',
-                allowBlank:false,
-                emptyText: '---请选择---',
-                displayField:'num', //要绑定到此ComboBox 的基础数据字段名称。
-                valueField:'nationalityType',
-                store:{
-                    type:'nationalityStore'
-                },
-                queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
-                minChars: 0, //用户在自动完成和typeAhead激活之前必须输入的最小字符数。
-                typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
-                forceSelection:true  //防止自由格式值并将其限制为列表中的项目
             },{
                 xtype:'fieldcontainer',
                 fieldLabel:'学生户口所在地',
+                labelWidth: 110,
                 name:'student_registeredResidence',
                 allowBlank:false,
+                msgTarget:'side',
                 combineErrors:true, // 字段容器将根据配置的msgTarget自动组合并显示其包含的所有字段中的验证错误，作为容器上的单个错误 
                 layout:'hbox',
                 defaults:{
@@ -149,10 +205,11 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
                         }
                     }
                 },{
+                    flex:1,
                     xtype:'combobox',
                     name:'provice',
                     id:'provice',
-                    allowBlank:false,
+                    // allowBlank:false,
                     emptyText:'---请选择省份---',
                     queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
                     typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
@@ -192,10 +249,11 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
                         }
                     }
                 },{
+                    flex:1,
                     xtype:'combobox',
                     name:'city',
                     id:'city',
-                    allowBlank:false,
+                    // allowBlank:false,
                     emptyText:'---请选择城市---',
                     queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
                     typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
@@ -229,10 +287,11 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
                         }
                     }
                 },{
+                    flex:1,
                     xtype:'combobox',
                     name:'county',
                     id:'county',
-                    allowBlank:false,
+                    // allowBlank:false,
                     emptyText:'---请选择地区---',
                     queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
                     typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
@@ -251,34 +310,17 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
                         }
                     }
                 },{
+                    flex:3,
                     xtype:'textfield',
                     name:'detailed',
+                    allowBlank:false,
+                    msgTarget:'side',
                     emptyText:'请填写详细地址'
                 }]
             },{
-                xtype:'combobox',
-                fieldLabel:'父母婚姻状况',
-                name:'student_parentalMarriage',
-                allowBlank:false,
-                emptyText: '---请选择---',
-                displayField:'name', //要绑定到此ComboBox 的基础数据字段名称。
-                valueField:'value',
-                store:Ext.create('Ext.data.Store',{
-                    fields:["name","value"],
-                    data:[
-                        {name:'未婚',value:'未婚'},
-                        {name:'已婚(有配偶)',value:'已婚'},
-                        {name:'丧偶',value:'丧偶'},
-                        {name:'离婚',value:'离婚'}
-                    ]
-                }),
-                queryMode: 'local', //ComboBox使用配置的Store的模式。默认remote,远程
-                minChars: 0, //用户在自动完成和typeAhead激活之前必须输入的最小字符数。
-                typeAhead: true, //如果匹配已知值，则填充并自动选择在可配置延迟（typeAheadDelay）之后键入的文本的其余部分。
-                forceSelection:true  //防止自由格式值并将其限制为列表中的项目
-            },{
                 xtype:'textfield',
                 fieldLabel:'学生健康智力情况',
+                labelWidth: 110,
                 name:'student_health'
             }]
         },{
@@ -322,10 +364,13 @@ Ext.define('Admin.view.onlineRegistration.ApplicationWindow',{
     },{
         xtype:'button',
         formBind:true, //在FormPanel内部时，formBind: true将根据表单的有效状态启用/禁用配置的任何组件。
+        bind:false,
+        reference: 'submitButton',
         scale: 'large',
         ui: 'soft-blue',
+        margin:'5 0',
         iconAlign: 'right',
-        iconCls: 'x-fa fa-bookmark-o',
+        iconCls: 'fa fa-bookmark-o',
         text:'保存',
         handler:'applicationSave'
     }]
